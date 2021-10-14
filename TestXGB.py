7e2bb15e6aa0c0d7d843abcee6cc6274a5224177
Arthur Vitui <avitui@redhat.com>
2021-10-14 15:46:35 -0400

    Added basic metrics to the wrapper class

--- TestXGB.py
+++ TestXGB.py
@@ -12,3 +12,10 @@ class TestXGB(object):
     resp = self.model.predict(pd.DataFrame(X))
     return resp
 
+
+  def metrics(self):
+    return [
+      {"type": "COUNTER", "key": "mycounter", "value": 1}, # a counter which will increase by the given value
+      {"type": "GAUGE", "key": "mygauge", "value": 100},   # a gauge which will be set to given value
+      {"type": "TIMER", "key": "mytimer", "value": 20.2},  # a timer which will add sum and count metrics - assumed millisecs
+    ]
