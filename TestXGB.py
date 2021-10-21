d6aaf71652529e6f8fceba673ba7375f8fd06432
Arthur Vitui <arthur.vitui@gmail.com>
2021-10-21 12:35:41 -0400

    Update TestXGB.py

--- TestXGB.py
+++ TestXGB.py
@@ -19,3 +19,22 @@ class TestXGB(object):
       {"type": "GAUGE", "key": "mygauge", "value": 100},   # a gauge which will be set to given value
       {"type": "TIMER", "key": "mytimer", "value": 20.2},  # a timer which will add sum and count metrics - assumed millisecs
     ]
+  
+  
+  def init_metadata(self):
+    meta = {
+            "name": "xgb-bne-time-series",
+            "versions": ["v-0.1"],
+            "platform": "seldon",
+            "inputs": [
+                {
+                    "messagetype": "ndarray",
+                    "schema": {"names": ["DURATION", "THREADS_1", "TPS", "queueProcess.allocating.processThreads", "queueProcess.allocating.queueSize", "queueProcess.bulk.batchSize", "queueProcess.bulk.processThreads",
+                                        "queueProcess.bulk.queueSize", "queueProcess.message.processThreads", "queueProcess.message.queueSize", "nb_immNodes"]},
+                }
+            ],
+            "outputs": [{"messagetype": "ndarray"}],
+            "custom": {"author": "eartvit", "extra": "test example"},
+    }
+    return meta
+  
