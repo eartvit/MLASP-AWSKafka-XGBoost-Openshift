a10eb8640aeb8328577ed116fd1c67a6a7205ac1
Arthur Vitui <avitui@redhat.com>
2022-04-29 01:04:35 -0400

    included feature names in model init

--- model/TestXGB.py
+++ model/TestXGB.py
@@ -5,11 +5,12 @@ import xgboost as xgb
 class TestXGB(object):
   def __init__(self):
     self.model = joblib.load("xgbModel.pkl")
+    self.features = ['BackgroundThreads', 'LogCleanerThreads', 'NumIoThreads','NumNetworkThreads', 'NumPartitions', 'NumNodes', 'NumReplicaFetchers','ThreadsClient', 'MessageSize']
 
 
   def predict(self, X, features_names):
-    print(f'Got X={X} of type {type(X)}, feature_names={features_names} \\n')
-    record = pd.DataFrame(X, columns=features_names)
+    print(f'Got X={X} of type {type(X)}, feature_names={self.features} \\n')
+    record = pd.DataFrame(X, columns=self.features)
     print(f'Record is {record}')
     resp = self.model.predict(record)
     return resp
